46032104641986667d2affb7894d68ac5094c20a
benjaminvickers <benjamin.vickers@yahoo.co.uk>
2016-01-23 18:59:16 +0000

    Removed unnecessary declaration

--- test.js
+++ test.js
@@ -1,9 +1,8 @@
 'use strict';
 
-var util = require('util');
 var FileWalker = require('./file-walker');
 var FileNode = require('./file-node');
 
 FileWalker(new FileNode(__dirname), function(err, rootFileNode) {
-  console.log(util.inspect(rootFileNode, {showHidden: false, depth:null}));
+  console.log(require('util').inspect(rootFileNode, {showHidden: false, depth:null}));
 });
