701c336b4ea271a1dd5af654ad9dd8d09a435a08
benjaminvickers <benjamin.vickers@yahoo.co.uk>
2016-01-24 10:33:35 +0000

    Removed unnecessary method nesting

--- file-walker.js
+++ file-walker.js
@@ -5,8 +5,12 @@ var path = require('path');
 var events = require('events');
 var FileNode = require('./file-node');
 
-var catfish = 'lemons';
+var rootNode;
+var totalWalksInProgress;
+var totalWalksComplete;
+var eventEmitter;
 
+// Module entry point.
 module.exports = function(opt, callback) {
   var opt = opt || {};
   
@@ -17,12 +21,12 @@ module.exports = function(opt, callback) {
   }
   
   // Variable readiness
-  var rootNode = new FileNode(opt.rootPath);
-  var totalWalksInProgress = 0;
-  var totalWalksComplete = 0;
+  rootNode = new FileNode(opt.rootPath);
+  totalWalksInProgress = 0;
+  totalWalksComplete = 0;
   
   // Setup events
-  var eventEmitter = new events.EventEmitter();
+  eventEmitter = new events.EventEmitter();
   eventEmitter.on('success', function() {
     callback(null, rootNode);
   });
@@ -39,63 +43,63 @@ module.exports = function(opt, callback) {
   catch(err) {
     eventEmitter.emit('error', err);
   }
-  
-  // Identifies all children of the FileNode passed in. Attaches the children
-  // to the FileNode. Sends each child to walkDirectory() to determine if the
-  // child is a directory and needs to be processed in the same way.
-  // @todo
-  // It is possible to pass in a file name, rather than a directory name.
-  // This needs to be fixed.
-  function walk(currentNode) {
-  
-    totalWalksInProgress++;
-    fs.readdir(currentNode.getName(), function(err, files) {
-      if(err) {
-        // Break out of the recursion.
-        throw err;
-      }
-      
-      // Store the file and directory names, breadth-first.
-      for(var i = 0; i < files.length; i++) {
-        var childNodePath = path.join(currentNode.getName(), files[i]);
-        var childNode = new FileNode(childNodePath);
-        currentNode.addChildNode(childNode);
-      }
-      
-      // Now walk the subdirectories.
-      var childNodes = currentNode.getChildNodes();
-      for(var i = 0; i < childNodes.length; i++) {
-        walkDirectory(childNodes[i]);
-      }
-      
-      if(isWalkCompleted())
-        eventEmitter.emit('success');
-    });
-  }
-  
-  // Checks the FileNode passed in to determine if it is a directory. If yes,
-  // then will call walk() to expand the directory contents.
-  function walkDirectory(childNode) {
-  
-    totalWalksInProgress++;
-    fs.stat(childNode.getName(), function(err, stats) {
-      if(stats.isDirectory()) {
-        walk(childNode);
-      }
-      
-      if(isWalkCompleted())
-        eventEmitter.emit('success');
-    });
-  }
-  
-  // Identifies whether all directories have been walked. If yes, then emits
-  // an event to complete the file walking operation.
-  function isWalkCompleted() {
-  
-    totalWalksComplete++;
-    if(totalWalksComplete == totalWalksInProgress) {
-      return true;
-    }
-    return false;
-  }
 };
+
+// Identifies all children of the FileNode passed in. Attaches the children
+// to the FileNode. Sends each child to walkDirectory() to determine if the
+// child is a directory and needs to be processed in the same way.
+// @todo
+// It is possible to pass in a file name, rather than a directory name.
+// This needs to be fixed.
+function walk(currentNode) {
+
+  totalWalksInProgress++;
+  fs.readdir(currentNode.getName(), function(err, files) {
+    if(err) {
+      // Break out of the recursion.
+      throw err;
+    }
+    
+    // Store the file and directory names, breadth-first.
+    for(var i = 0; i < files.length; i++) {
+      var childNodePath = path.join(currentNode.getName(), files[i]);
+      var childNode = new FileNode(childNodePath);
+      currentNode.addChildNode(childNode);
+    }
+    
+    // Now walk the subdirectories.
+    var childNodes = currentNode.getChildNodes();
+    for(var i = 0; i < childNodes.length; i++) {
+      walkDirectory(childNodes[i]);
+    }
+    
+    if(isWalkCompleted())
+      eventEmitter.emit('success');
+  });
+}
+
+// Checks the FileNode passed in to determine if it is a directory. If yes,
+// then will call walk() to expand the directory contents.
+function walkDirectory(childNode) {
+
+  totalWalksInProgress++;
+  fs.stat(childNode.getName(), function(err, stats) {
+    if(stats.isDirectory()) {
+      walk(childNode);
+    }
+    
+    if(isWalkCompleted())
+      eventEmitter.emit('success');
+  });
+}
+
+// Identifies whether all directories have been walked. If yes, then emits
+// an event to complete the file walking operation.
+function isWalkCompleted() {
+
+  totalWalksComplete++;
+  if(totalWalksComplete == totalWalksInProgress) {
+    return true;
+  }
+  return false;
+}
